ce02e5d5f522432d8207eb474c10e5ebe6247ef7
Oladoye-Ajiboye4 <oladoyeajiboye@gmail.com>
2026-02-12 12:40:38 +0100

    Refactor state retrieval endpoint and update response structure

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require('express');
 const nigerianStates = require('./states');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 6060; 
+const port = process.env.PORT || 6060;
 
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+app.get('/:name', (req, res) => {
+  const stateName = req.params.name.toLowerCase();
+  const state = nigerianStates.find(s => s.name.toLowerCase() === stateName);
+  if (state) {
+    res.status(200).json({ message: `Information about ${state.name} State`, data: state, status: true });
+  } else {
+    res.status(404).json({ message: "State not found", data: null, status: false });
+  }
+})
 
-app.get('/states', (req, res) => {
-  res.send(nigerianStates)
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "List of Nigerian States", states: nigerianStates, status: true });
 })
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
